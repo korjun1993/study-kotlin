318c0be735bd89b5b10c05dfff28f1d8be407c34
korjun1993 <corojoon93@naver.com>
2023-12-28 23:56:55 +0900

    실습: 데코레이터패턴

---
+++ src/main/kotlin/ch04/위임/01_데코레이터패턴.kt
@@ -0,0 +1,35 @@
+package ch04.위임
+
+/**
+ * 데코레이터 패턴
+ * 기본 제품에 포장지나 외부 디자인을 변경함으로써 새로운 기능을 부여하는 것과 같이,
+ * 객체 지향 프로그래밍에서 원본 객체에 대한 무언가를 장식하여 더 멋진 기능을 가지게 만드는 방법
+ * 데코레이터 패턴을 이용하면 필요한 추가 기능의 조합을 런타임에서 동적으로 생성할 수 있다.
+ */
+
+/**
+ * 원본 객체와 장식된 객체 모두를 묶는 역할
+ */
+interface IComponent {
+    fun operation()
+}
+
+/**
+ * 장식될 원본 객체
+ */
+class ConcreteComponent : IComponent {
+    override fun operation() {
+        //...
+    }
+}
+
+/**
+ * 추상화된 장식자 클래스
+ * wrapee: 원본 객체를 composition
+ */
+abstract class Decorator(val wrapee: IComponent) : IComponent {
+    override fun operation() {
+        wrapee.operation() // 위임
+    }
+}
+
